f7f3f25adef28d20da06b8e869d95b8e59f48694
aminggs <aminggs@yahoo.co.uk>
2010-10-15 23:15:15 +0200

    Generate XML output via JAXP Transformer; not really parsing anything yet

--- src/com/inggs/markup/MarkupDriver.java
+++ src/com/inggs/markup/MarkupDriver.java
@@ -1,15 +1,43 @@
 package com.inggs.markup;
 
 import com.inggs.markup.parser.MarkupReader;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.sax.SAXSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 /**
  * @author Andrew Inggs
  */
 public class MarkupDriver {
 
-    public static void main(String[] args) {
-        MarkupReader markupParser = new MarkupReader(System.in);
-        markupParser.parse();
+    public static void main(String[] args) throws TransformerException, SAXException, FileNotFoundException {
+        InputStream inputStream;
+        if (args.length > 0) {
+            inputStream = new FileInputStream(args[0]);
+        } else {
+            inputStream = System.in;
+        }
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer(); // identity transformer
+        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+        Source xmlSource = new SAXSource(new MarkupReader(), new InputSource(inputStream));
+        Result result = new StreamResult(new BufferedOutputStream(System.out));
+        transformer.transform(xmlSource, result);
     }
 
 }
--- src/com/inggs/markup/parser/MarkupReader.java
+++ src/com/inggs/markup/parser/MarkupReader.java
@@ -1,50 +1,169 @@
 package com.inggs.markup.parser;
 
+import org.xml.sax.Attributes;
 import org.xml.sax.ContentHandler;
+import org.xml.sax.DTDHandler;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXNotRecognizedException;
+import org.xml.sax.SAXNotSupportedException;
+import org.xml.sax.XMLReader;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 /**
  * @author Andrew Inggs
  */
-public class MarkupReader {
+public class MarkupReader implements XMLReader {
 
-    private InputStream inputStream;
+    private static final String EMPTY_NAMESPACE_URI = "";
+
+    private static final Attributes EMPTY_ATTRIBUTES = new EmptyAttributesImpl();
+
+    private MarkupTokenizer tokenizer;
 
     private ContentHandler contentHandler;
-
-    // setters and getters
-
-    public void setInputStream(InputStream inputStream) {
-        this.inputStream = inputStream;
-    }
-
-    public void setContentHandler(ContentHandler contentHandler) {
-        this.contentHandler = contentHandler;
-    }
 
     // constructors
 
     public MarkupReader() {
     }
 
-    public MarkupReader(InputStream inputStream) {
-        this.inputStream = inputStream;
+    // XMLReader methods
+
+    public boolean getFeature(String name) throws SAXNotRecognizedException, SAXNotSupportedException {
+        throw new UnsupportedOperationException();
     }
 
-    public MarkupReader(ContentHandler contentHandler) {
-        this.contentHandler = contentHandler;
+    public void setFeature(String name, boolean value) throws SAXNotRecognizedException, SAXNotSupportedException {
+        // do nothing
     }
 
-    public MarkupReader(InputStream inputStream, ContentHandler contentHandler) {
-        this.inputStream = inputStream;
-        this.contentHandler = contentHandler;
+    public Object getProperty(String name) throws SAXNotRecognizedException, SAXNotSupportedException {
+        throw new UnsupportedOperationException();
     }
 
-    // ordinary methods
+    public void setProperty(String name, Object value) throws SAXNotRecognizedException, SAXNotSupportedException {
+        // do nothing
+    }
 
-    public void parse() {
+    public void setEntityResolver(EntityResolver resolver) {
+        throw new UnsupportedOperationException();
+    }
 
+    public EntityResolver getEntityResolver() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void setDTDHandler(DTDHandler handler) {
+        throw new UnsupportedOperationException();
+    }
+
+    public DTDHandler getDTDHandler() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void setContentHandler(ContentHandler handler) {
+        this.contentHandler = handler;
+    }
+
+    public ContentHandler getContentHandler() {
+        return this.contentHandler;
+    }
+
+    public void setErrorHandler(ErrorHandler handler) {
+        throw new UnsupportedOperationException();
+    }
+
+    public ErrorHandler getErrorHandler() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void parse(InputSource input) throws IOException, SAXException {
+        tokenizer = new MarkupTokenizer(input.getByteStream());
+        contentHandler.startDocument();
+        body();
+        contentHandler.endDocument();
+    }
+
+    public void parse(String systemId) throws IOException, SAXException {
+        throw new UnsupportedOperationException();
+    }
+
+    // parsing methods
+
+    private void body() throws SAXException {
+        emitStart("body");
+        emitEnd("body");
+    }
+
+    // utility methods
+
+    private void emitStart(String tagName) throws SAXException {
+        contentHandler.startElement(EMPTY_NAMESPACE_URI, tagName, tagName, EMPTY_ATTRIBUTES);
+    }
+
+    private void emitEnd(String tagName) throws SAXException {
+        contentHandler.endElement(EMPTY_NAMESPACE_URI, tagName, tagName);
+    }
+}
+
+/**
+ * This is a do-nothing helper class (implementing the SAX Attributes interface)
+ * that throws UnsupportedOperationExceptions for all methods except {@link
+ * #getLength()}, which returns 0 (zero).
+ */
+class EmptyAttributesImpl implements Attributes {
+
+    public int getLength() {
+        return 0;
+    }
+
+    public String getURI(int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    public String getLocalName(int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    public String getQName(int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    public String getType(int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    public String getValue(int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getIndex(String uri, String localName) {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getIndex(String qName) {
+        throw new UnsupportedOperationException();
+    }
+
+    public String getType(String uri, String localName) {
+        throw new UnsupportedOperationException();
+    }
+
+    public String getType(String qName) {
+        throw new UnsupportedOperationException();
+    }
+
+    public String getValue(String uri, String localName) {
+        throw new UnsupportedOperationException();
+    }
+
+    public String getValue(String qName) {
+        throw new UnsupportedOperationException();
     }
 
 }
